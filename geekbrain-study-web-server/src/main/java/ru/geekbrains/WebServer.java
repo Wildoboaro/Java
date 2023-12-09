722b9815078407f79d7b38a3c80b5c37192e5145
P.Bor <wildoboaro@gmail.com>
2023-12-09 19:34:25 +0300

    add first simple server on java

---
+++ geekbrain-study-web-server/src/main/java/ru/geekbrains/WebServer.java
@@ -0,0 +1,51 @@
+package ru.geekbrains;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class WebServer
+{
+    public static void main(String[] args)
+    {
+        try (ServerSocket severSocket = new ServerSocket( 8088 ))
+        {
+            while(true)
+            {
+                Socket socket = severSocket.accept();
+                System.out.println("Yeah! New client connected.");
+                BufferedReader input = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+
+                PrintWriter output =  new PrintWriter(socket.getOutputStream());
+
+                while (!input.ready());
+
+                while (input.ready())
+                {
+                    System.out.println(input.readLine());
+                }
+
+                output.println("HTTP/1.1 200 OK");
+                output.println("Content-Type: text/html; charset=utf-8");
+                output.println();
+                output.println("<h1>Hi, it is first server.</h1>");
+                output.flush();
+
+                input.close();
+                output.close();
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
